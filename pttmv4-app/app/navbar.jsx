a69c90209fc9538335313c261655e8c0355d9f51
John Ramos <jramos5883@gmail.com>
2023-07-19 11:15:20 -0700

    Added next/font and completed navbar for now, going to start on homepage

--- pttmv4-app/app/navbar.jsx
+++ pttmv4-app/app/navbar.jsx
@@ -1,13 +1,36 @@
 import Link from "next/link";
 
+import { Oleo_Script } from "@next/font/google";
+import { Luckiest_Guy } from "@next/font/google";
+
+const oleoScript = Oleo_Script({
+  weight: "700",
+  subsets: ["latin"],
+});
+
+const luckietGuy = Luckiest_Guy({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Navbar() {
   return (
-    <div className="bg-gray-200 h-16 w-full">
-      <h1 className="text-2xl">Navbar!</h1>
-      <Link href="/">Home Page</Link>
-      <Link href="/aboutus">About Us</Link>
-      <Link href="/menu">Menu</Link>
-      <Link href="/location">Location</Link>
+    <div className="px-4 bg-gray-200 h-16 w-full flex flex-row justify-between items-center">
+      <Link className="text-3xl" href="/">
+        <p className={oleoScript.className}>Home</p>
+      </Link>
+      <Link className="text-3xl" href="/aboutus">
+        <p className={oleoScript.className}>About Us</p>
+      </Link>
+      <h1 className={luckietGuy.className}>
+        <span className="text-5xl text-bold">- POKE TO THE MOON -</span>
+      </h1>
+      <Link className="text-3xl" href="/menu">
+        <p className={oleoScript.className}>Menu</p>
+      </Link>
+      <Link className="text-3xl" href="/location">
+        <p className={oleoScript.className}>Location</p>
+      </Link>
     </div>
   );
 }
